1c8f60123ad2092f47bf6c6281813eb24a2aa533
Sachin <sachin.mishra@datasociety.com>
2023-07-07 14:34:25 +0530

    testing

--- src/assesment_creator/get_assesment.py
+++ src/assesment_creator/get_assesment.py
@@ -1,12 +1,13 @@
 from assesment_creator.logger import logger
 from assesment_creator.cutom_exception import InvalidURLException
-from assesment_creator.helper import *
+from assesment_creator.helper import output_file
 from ensure import ensure_annotations
-
+import re
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
-
+import docx
+import os
 #url= "https://docs.google.com/forms/d/1WLfwaz1fuiueMGE8iEigwjZSP2HEUPGlyIWE9QNYhpg/edit"
 
 
@@ -16,28 +17,36 @@ def get_data(link: str) -> list:
     sleep(3)
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.find_all(class_='geS5n')
-    content= [pair.text for pair in content]
-    return content
+    content= [pair for pair in content]
+    content= str(content)
+    mixed_pattern= 'M7eMe">(.*?)\<|auto">(.*?)\<\/span>'
+    qno_list = re.findall(mixed_pattern, content)
+    return qno_list
 
 @ensure_annotations
+def create_docx(qno_list:list, output_file:str = "assessment.docx") -> None:
+
+    doc = docx.Document()
+    #Add a Title to the document
+    doc.add_heading('Assesment', 0)
+    for i, qno in enumerate(qno_list):
+        if len(qno[0])!=0:
+            doc.add_paragraph(str(qno[0]))
+        else:
+            doc.add_paragraph(str(qno[1]))
+    os.makedirs("data", exist_ok=True)
+    doc.save(os.path.join(os.path.curdir, "data", output_file))
+
+
 def create_assesment(form_link:str, file_name:str) -> None:
-    global output_file = ""
     try:
         if form_link and file_name is None:
             raise InvalidURLException("Url or file_name cannot be empty")
         else:
-            splitted_file_name = file_name.split(".")
-
-            if "." in file_name and splitted_file_name[-1] != "docx" : 
-                output_file = splitted_file_name[0] + ".docx"
-
-            elif "." not in file_name:
-                output_file = file_name + ".docx"
-
-            content= get_data(form_link)
-            if content:
-                questions, options= filter_qno(content)
-                create_docx(questions, options, file_name)
+            output_file(file_name)
+            content_list= get_data(form_link)
+            if content_list:
+                create_docx(content_list, file_name)
                 print(f"Scraping done successfully! Check {file_name}\nThank you! \\m/")
             else:
                 print("No data found! Please check the link provided")   
--- src/assesment_creator/helper.py
+++ src/assesment_creator/helper.py
@@ -1,37 +1,16 @@
 from ensure import ensure_annotations
-import docx
-import os
 
-@ensure_annotations
-def filter_text(text:str) -> tuple[str, list] :
-    text_list = []
 
-    text_list = text.split("\n")
-    text_list = [t for t in text_list if "1 point" not in t and "5 points" not in t and t!="*"]
 
-    return text_list[0], text_list[1:]
+def output_file(file_name:str) -> str:
+    output_file= ""
+    splitted_file_name = file_name.split(".")
 
-@ensure_annotations
-def filter_qno(raw_qno:list) -> tuple[list, list]:
-    questions = []
-    options = []
-    for text in raw_qno:
-        q, a = filter_text(text)
-        questions.append(q)
-        options.append(a)
+    if "." in file_name and splitted_file_name[-1] != "docx" : 
+        output_file = splitted_file_name[0] + ".docx"
 
-    return questions, options
+    elif "." not in file_name:
+        output_file = file_name + ".docx"
+    return output_file
 
-@ensure_annotations
-def create_docx(questions:list, options:list, output_file:str = "assessment.docx") -> None:
 
-    doc = docx.Document()
-
-    for question, options in zip(questions,options):
-        doc.add_paragraph(question)
-        for option in options:
-            doc.add_paragraph(option)
-        doc.add_paragraph("\n")
-
-    os.makedirs("data", exist_ok=True)
-    doc.save(os.path.join(os.path.curdir, "data", output_file))
